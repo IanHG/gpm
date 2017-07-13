7c55172eee5a34398aff4d93d45186c9e72b38a8
Ian Godtliebsen <ian@chem.au.dk>
2017-07-13 14:16:52 +0200

    Initial working version with reduced feature set
    Can now build packages and create lmod lua scripts
    
    Prerquisites are missing, but are in the making ;)

--- build-package.lua
+++ build-package.lua
@@ -25,6 +25,8 @@ config = {
    gpk_directory = lfs.currentdir() .. "/gpk",
    base_build_directory = "/tmp/",
    nprocesses = 20,
+   lmod_directory = "/comm/modulefiles",
+   heirarchical = {},
 }
 
 -------------------------------------
@@ -46,6 +48,22 @@ function get_version()
 end
 
 -------------------------------------
+-- Is pkgtype a hierarchical one?
+--
+-- @param{String} pkgtype
+--
+-- @param{Boolean} 
+-------------------------------------
+function is_heirarchical(pkgtype)
+   for key,value in pairs(config.heirarchical) do
+      if pkgtype == value then
+         return true
+      end
+   end
+   return false
+end
+
+-------------------------------------
 -- Dump args to stdout.
 --
 -- @param{Dictionary} args The argument dictionary to print.
@@ -55,6 +73,20 @@ function dump_args(args)
       print(key .. " = " .. tostring(value))
    end
 end
+
+-------------------------------------
+-- Copy a file
+-------------------------------------
+function copy_file(src, dest)
+   infile = io.open(src, "r")
+   instr = infile:read("*a")
+   infile:close()
+
+   outfile = io.open(dest, "w")
+   outfile:write(instr)
+   outfile:close()
+end
+
 
 -------------------------------------
 -- Read GPM package file (GPK).
@@ -90,17 +122,33 @@ function bootstrap_package(args)
    local f, msg = loadfile(filepath)
    if f then
       f()
-      package.definition = definition
-      package.prequisites = prerequisites
-      package.build = build
-      package.lmod = lmod
    else
       error("Error loading package. Reason : '" .. msg .. "'.")
    end
-
+   
+   package.description = description
+   package.definition = definition
+   package.prequisites = prerequisites
+   package.build = build
+   package.lmod = lmod
+   
    package.definition.pkgversion = args.pkv
    package.definition.pkg = package.definition.pkgname .. "-" .. package.definition.pkgversion
+   
+   pkginstall = path.join(config.install_directory, package.definition.pkggroup)
+   --[[
+   if is_heirarchical(package.definition.pkggroup) then
+      for key,prereq in pairs(package.prerequisite) do
+         pkginstall = path.join(pkginstall, args.prerequisite[prereq])
+      end
+   end
+   --]]
+   pkginstall = path.join(path.join(pkginstall, package.definition.pkgname), package.definition.pkgversion)
+   package.definition.pkginstall = pkginstall
+   
    package.definition.nprocesses = config.nprocesses
+
+   package.build_directory = path.join(config.base_build_directory, "build-" .. package.definition.pkg)
 
    return package
 end
@@ -136,42 +184,89 @@ end
 -- Build the package.
 --
 -- @param package
--- @param build
+-------------------------------------
+function build_package(package)
+   -- Download package
+   for line in string.gmatch(package.build.source, ".*$") do
+      line = substitute_placeholders(package.definition, line)
+      execute_command(line)
+   end
+   
+   -- Build package
+   package_directory = path.join(package.build_directory, package.definition.pkg)
+   lfs.chdir(package_directory)
+   for line in string.gmatch(package.build.command, ".*$") do
+      line = substitute_placeholders(package.definition, line)
+      execute_command(line)
+   end
+end
+
+-------------------------------------
+-- Generate Lmod script.
+--
+-- @param package
 --
 -- @return{Boolean}
 -------------------------------------
-function build_package(package)
-   exception.try(function()
-         -- try block
-         build_directory = path.join(config.base_build_directory, "build-" .. package.definition.pkg)
-         package_directory = path.join(build_directory, package.definition.pkg)
+function build_lmod_modulefile(package)
+   lmod_filename = path.join(package.build_directory, package.definition.pkgversion .. ".lua")
+   lmod_file = io.open(lmod_filename, "w")
 
-         lfs.rmdir(build_directory)
-         lfs.mkdir(build_directory)
+   lmod_file:write("-- -*- lua -*-\n")
+   lmod_file:write("help(\n")
+   lmod_file:write("[[\n")
+   lmod_file:write(package.lmod.help .. "\n")
+   lmod_file:write("]])\n")
+   lmod_file:write("------------------------------------------------------------------------\n")
+   lmod_file:write("-- This file was generated automagically by Grendel Package Manager (GPM)\n")
+   lmod_file:write("------------------------------------------------------------------------\n")
+   lmod_file:write("-- Description\n")
+   lmod_file:write("whatis([[\n")
+   lmod_file:write(package.description)
+   lmod_file:write("]])\n")
+   lmod_file:write("\n")
+   lmod_file:write("-- Set family\n")
+   lmod_file:write("local fam = \"" .. package.definition.pkgfamily .. "\"\n")
+   lmod_file:write("family(fam)\n")
+   lmod_file:write("\n")
+   lmod_file:write("-- Basic module setup\n")
+   lmod_file:write("local version     = myModuleVersion()\n")
+   lmod_file:write("local name        = myModuleName()\n")
+   lmod_file:write("local fileName    = myFileName()\n")
+   lmod_file:write("local nameVersion = pathJoin(name, version)\n")
+   lmod_file:write("local installDir  = pathJoin(os.getenv(\"GRENDEL_COMM_CORE_PATH\"), nameVersion)\n")
+   lmod_file:write("\n")
+   lmod_file:write("-- Compiler optional modules setup\n")
+   lmod_file:write("local dir = pathJoin(fam, nameVersion)\n")
+   lmod_file:write("prepend_path('MODULEPATH', pathJoin(os.getenv(\"MODULEPATH_ROOT\"), dir))\n")
+   lmod_file:write("\n")
+   
+   lmod_file:write("-- Package specific\n")
+   
+   -- Do all setenv
+   if package.lmod.setenv then
+      for key,value in pairs(package.lmod.setenv) do
+         lmod_file:write("setenv('" .. value[1] .. "', pathJoin(installDir, '" .. value[2] .. "'))\n")
+      end
+   end
+   
+   -- Do all prepend_path
+   if package.lmod.prepend_path then
+      for key,value in pairs(package.lmod.prepend_path) do
+         lmod_file:write("prepend_path('" .. value[1] .. "', pathJoin(installDir, '" .. value[2] .. "'))\n")
+      end
+   end
 
-         -- Download package
-         lfs.chdir(build_directory)
-         for line in string.gmatch(package.build.source, ".*$") do
-            line = substitute_placeholders(package.definition, line)
-            execute_command(line)
-         end
-         
-         -- Build package
-         lfs.chdir(package_directory)
-         for line in string.gmatch(package.build.command, ".*$") do
-            line = substitute_placeholders(package.definition, line)
-            execute_command(line)
-         end
-      end, function(e)
-         -- catch block
-         print ("exeption caught : " .. e)
-         print ("but i cannot print here :CC")
-         lfs.chdir(config.current_directory)
-         status = lfs.rmdir(build_directory)
-         if not status then
-            print("did not rm dir :C")
-         end
-      end)
+   -- Close file after wirting it
+   lmod_file:close()
+
+   -- Put the file in the correct place
+   if false then
+   else
+      modulefile_directory = path.join(path.join(config.lmod_directory, package.definition.pkggroup), package.definition.pkgname)
+      lmod_filename_new = path.join(modulefile_directory, package.definition.pkgversion .. ".lua")
+      copy_file(lmod_filename, lmod_filename_new)
+   end
 end
 
 
@@ -188,6 +283,8 @@ function main()
    )
    parser:option("--pkv", "Set Package Version (PKV) of the package to install."):overwrite(false)
    parser:option("-c --config", "Provide config file."):overwrite(false)
+   parser:flag("--nobuild", "Do not build package, only create Lmod script.")
+   parser:flag("--cleanup", "Cleanup by removing build directory after build is complete.")
    parser:flag("-v --version", "Print '" .. get_version() .. "' and exit."):action(function()
       print(get_version())
       os.exit(0)
@@ -196,17 +293,44 @@ function main()
    args = parser:parse()
    dump_args(args)
    
-   -- Bootstrap build
+   -- Try the build
    exception.try(function() 
+      -- Bootstrap build
       config = bootstrap_config(args, config)
       package = bootstrap_package(args)
+
+      -- Create build dir
+      lfs.rmdir(package.build_directory)
+      lfs.mkdir(package.build_directory)
+      lfs.chdir(package.build_directory)
+      
+      -- Do the build
+      if not args.nobuild then
+         build_package(package)
+      end
+
+      -- Create Lmod file
+      build_lmod_modulefile(package)
+      
+      -- Change back to calling dir
+      lfs.chdir(config.current_directory)
+      
+      -- Remove build dir
+      if args.cleanup then
+         status, msg = lfs.rmdir(build_directory)
+         print("Did not remove build directory. Reason : '" .. msg .. "'.") 
+      end
    end, function(e)
       exception.message(e)
+      --[[
+      status, msg = lfs.rmdir(build_directory)
+      if not status then
+         print("did not rm dir :C")
+      end
+      --]]
       print("\n" .. parser:get_usage())
    end)
    
-   -- Do the build
-   build_package(package)
 end
 
 -- Run main driver.
--- config.lua
+++ config.lua
@@ -1,4 +1,16 @@
+-- GPM config file
 config = {
+   -- Path to search for gpk files
    gpk_directory = "/comm/build/gpk",
-   base_build_directory = "/tmp",
+   
+   -- Temporary build directory
+   --base_build_directory = "/tmp",
+   base_build_directory = "/scratch/ian",
+
+   -- Install directory
+   install_directory = "/comm",
+
+   -- Modulefiles setup
+   lmod_directory = "/comm/modulefiles",
+   heirarchical = {"core"},
 }
