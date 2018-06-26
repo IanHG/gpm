7d933df3d36b277fdc0762874fba17ce70899983
Ian Heide Godtliebsen <ian@chem.au.dk>
2018-06-26 10:33:56 +0200

    updated

--- lib/isa.lua
+++ lib/isa.lua
@@ -41,21 +41,27 @@ local function trim(s)
 end
 
 -- Vector instruction set dictionary
-local uid = -1
-local function get_uid_greater()
-   uid = uid + 1
-   return uid
+local function generate_vector_instruction_set_dictionary()
+   local uid = -1
+   local function get_uid_greater()
+      uid = uid + 1
+      return uid
+   end
+
+   local vector_instruction_set_dictionary = {}
+   vector_instruction_set_dictionary["unknown"] = get_uid_greater()
+   vector_instruction_set_dictionary["sse" ]    = get_uid_greater()
+   vector_instruction_set_dictionary["sse2"]    = get_uid_greater()
+   vector_instruction_set_dictionary["sse3"]    = get_uid_greater()
+   vector_instruction_set_dictionary["sse4"]    = get_uid_greater()
+   vector_instruction_set_dictionary["avx"]     = get_uid_greater()
+   vector_instruction_set_dictionary["avx2"]    = get_uid_greater()
+   vector_instruction_set_dictionary["avx512"]  = get_uid_greater()
+
+   return vector_instruction_set_dictionary
 end
 
-local vector_instruction_set_dictionary = {}
-vector_instruction_set_dictionary["unknown"] = get_uid_greater()
-vector_instruction_set_dictionary["sse" ]    = get_uid_greater()
-vector_instruction_set_dictionary["sse2"]    = get_uid_greater()
-vector_instruction_set_dictionary["sse3"]    = get_uid_greater()
-vector_instruction_set_dictionary["sse4"]    = get_uid_greater()
-vector_instruction_set_dictionary["avx"]     = get_uid_greater()
-vector_instruction_set_dictionary["avx2"]    = get_uid_greater()
-vector_instruction_set_dictionary["avx512"]  = get_uid_greater()
+local vector_instruction_set_dictionary = generate_vector_instruction_set_dictionary()
 
 --- Get number of cores
 --
@@ -99,12 +105,74 @@ local function set_flags(isa_local_flags, flags)
       isa_local_flags.vec_is_level = vector_instruction_set_dictionary["sse"]
    end
 
-   if flags:match("fma") then
-      isa_local_flags.fma = true
+   for _, flag in pairs(split(flags, " ")) do
+      isa_local_flags[flag] = true
    end
 
-   if flags:match("lm") then
-      isa_local_flags.lm  = true
+   if isa_local_flags["bmi1"] then
+      isa_local_flags["bmi"] = true
+   end
+
+   if isa_local_flags["ssse3"] then
+      isa_local_flags["sse3"] = true
+   end
+
+   if isa_local_flags["pclmulqdq"] then
+      isa_local_flags["pclmul"] = true
+   end
+
+   if isa_local_flags["3dnowprefetch"] then
+      isa_local_flags["prefetchw"] = true
+   end
+end
+
+local function detect_intel_cpu_type(isa_local)
+   local uid = -1
+   local function get_uid_greater()
+      uid = uid + 1
+      return uid
+   end
+
+   local intel_cpu_types = { }
+   intel_cpu_types[get_uid_greater()] = { 
+         cpu_type = "skylake", 
+         flags =  {  "movbe", "mmx", "sse", "sse2", "sse3", "ssse3", "sse4_1", "sse4_2", 
+                     "popcnt", "pku", "avx", "avx2", "aes", "pclmul", "fsgsbase", "rdrand", "fma", 
+                     "bmi", "bmi2", "f16c", "rdseed", "adx", "prefetchw", "clflushopt", 
+                     "xsavec", "xsaves", "avx512f", "clwb", "avx512vl", "avx512bw", "avx512dq", "avx512cd"
+                  }
+            }
+   intel_cpu_types[get_uid_greater()] = { 
+         cpu_type = "skylake", 
+         flags =  {  "movbe", "mmx", "sse", "sse2", "sse3", "ssse3", "sse4_1", "sse4_2", 
+                     "popcnt", "avx", "avx2", "aes", "pclmul", "fsgsbase", "rdrand", "fma", 
+                     "bmi", "bmi2", "f16c", "rdseed", "adx", "prefetchw", "clflushopt", 
+                     "xsavec", "xsaves"
+                  }
+            }
+
+   
+   for i = 1, #intel_cpu_types do
+      cpu_type_dict = intel_cpu_types[i]
+      found = true
+      for _, value in pairs(cpu_type_dict.flags) do
+         if not isa_local.flags[value] then
+            print("breaking at : " .. value)
+            found = false
+            break
+         end
+      end
+      if found then
+         print(cpu_type_dict.cpu_type)
+         isa_local.cpu_type = cpu_type_dict.cpu_type
+         break
+      end
+   end
+end
+
+local function detect_cpu_type(isa_local)
+   if isa_local.vendor_id == "GenuineIntel" then
+      detect_intel_cpu_type(isa_local)
    end
 end
 
@@ -112,12 +180,11 @@ local isa_class = class.create_class()
 
 function isa_class:__init()
    self.ncores    = 0
-   self.vendor_id = "UNKNOWN"
+   self.vendor_id = "unknown"
+   self.cpu_type  = "unknown"
    self.flags = {
       vec_is       = "unknown",
       vec_is_level = vector_instruction_set_dictionary["unknown"],
-      fma          = false,
-      lm           = false,
    }
 end
 
@@ -126,6 +193,7 @@ function isa_class:print()
    print("   Ncores : " .. self.ncores)
    print("   Vendor : " .. self.vendor_id)
    print("   VECIS  : " .. self.flags.vec_is)
+   print("   cpu_type  : " .. self.cpu_type)
    if self.flags.lm then
       print("   BIT    : 64")
    else
@@ -145,8 +213,8 @@ local gpu_class = class.create_class()
 
 function gpu_class:__init()
    self.ngpu   = -1
-   self.vendor = "UNKNOWN"
-   self.model  = "UNKNOWN"
+   self.vendor = "unknown"
+   self.model  = "unknown"
 end
 
 function gpu_class:print()
@@ -174,6 +242,21 @@ function gpus_class:print()
    if self.ngpus == 0 then
       print("No GPUs found")
    end
+end
+
+local mem_class = class.create_class()
+
+function mem_class:__init()
+   self.mem_available = "UNKNOWN"
+   self.mem_total     = "UNKNOWN"
+   self.mem_free      = "UNKNOWN"
+end
+
+function mem_class:print()
+   print("MEMINFO:")
+   print("   MEM_TOTAL     : " .. self.mem_total)
+   print("   MEM_AVAILABLE : " .. self.mem_available)
+   print("   MEM_FREE      : " .. self.mem_free)
 end
 
 --- Find and identify nvidia gpus.
@@ -240,6 +323,8 @@ local function deduce_isa()
       end
    end
 
+   detect_cpu_type(isa_local)
+
    return isa_local
 end
 
@@ -270,8 +355,36 @@ local function deduce_gpus()
    return gpus_local
 end
 
+---
+--
+local function deduce_mem()
+   local mem_local = mem_class:create({})
+   
+   local pipe0   = io.popen("cat /proc/meminfo")
+   local meminfo = pipe0:read("*all") or "0"
+   pipe0:close()
+
+   for line in meminfo:gmatch(".-\n") do
+      line = line:gsub("\n", "")
+      
+      -- Parse key/value pairs
+      local key, value = key_value_pair(line)
+      
+      if key:match("MemTotal") then
+         mem_local.mem_total = value
+      elseif key:match("MemAvailable") then
+         mem_local.mem_available = value
+      elseif key:match("MemFree") then
+         mem_local.mem_free = value
+      end
+   end
+
+   return mem_local
+end
+
 -- Load module
 M.deduce_isa  = deduce_isa
 M.deduce_gpus = deduce_gpus
+M.deduce_mem  = deduce_mem
 
 return M
