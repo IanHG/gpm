5341ae42af9341c1e55f816f8e132bb7ae441159
Ian Heide Godtliebsen <ian@chem.au.dk>
2019-11-27 12:32:49 +0100

    fixed bug with writing lmod file

--- lib/installer.lua
+++ lib/installer.lua
@@ -569,7 +569,7 @@ function lmod_installer_class:write_modulefile()
    for k, v in pairs(self.gpack.lmod.family) do
       self.modulefile:write("family(\"" .. v[1] .. "\")\n")
       if first then
-         "fam = " .. v[1]
+         self.modulefile:write("fam = " .. v[1] .. "\n")
       end
       first = false
    end
